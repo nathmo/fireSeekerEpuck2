8e963d011cf5b55155fe971c2d948dcdd049b352
Fel36 <felipe1612@icloud.com>
2024-04-29 19:32:28 +0200

    add .h un peu partout

--- src/IR_proximity.h
+++ src/IR_proximity.h
@@ -7,3 +7,22 @@
  * This file contains the function header related to the IR proximity sensor.
  * We detect when we are about to hit an obstacle and the angle of the collision
  */
+
+#ifndef IR_PROXIMITY_H
+#define IR_PROXIMITY_H
+
+#include <ch.h>
+#include <hal.h>
+#include <sensors/proximity.h>
+
+// Déclaration des sémaphores pour la détection de collision par les capteurs IR
+extern BSEMAPHORE_DECL(sem_detection_collision_side_frontR, FALSE);
+extern BSEMAPHORE_DECL(sem_detection_collision_side_frontL, FALSE);
+extern BSEMAPHORE_DECL(sem_detection_collision_side_R, FALSE);
+extern BSEMAPHORE_DECL(sem_detection_collision_side_L, FALSE);
+extern BSEMAPHORE_DECL(sem_detection_collision_NO, TRUE);
+
+// Fonction pour démarrer le traitement de la proximité IR
+void process_IR_proximity_start(void);
+
+#endif /* IR_PROXIMITY_H */
--- src/movement.h
+++ src/movement.h
@@ -8,3 +8,22 @@
  * such as the high level patrol and rushFire and the lower level fonction like
  * to rotate of a given angle or to proceed in straight line at a given speed.
  */
+
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+#include <stdint.h>
+
+// Fonction pour orienter le robot vers un capteur spécifique
+void orientation_sensor(uint8_t sensor_index);
+
+// Fonction pour tourner d'un angle spécifique (en degrés)
+void turn_specific_angle(int16_t angle_degrees);
+
+// Fonction pour faire avancer le robot à une vitesse donnée
+void avancer(int16_t vitesse);
+
+// Fonction pour arrêter les moteurs du robot
+void stop_engines();
+
+#endif /* MOVEMENT_H */
