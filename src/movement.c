d360b340644994c9cd843eeaf189c9b3235ed464
Nathann Morand <natmo@hotmail.ch>
2024-06-05 18:59:34 +0200

    Update movement.c

--- src/movement.c
+++ src/movement.c
@@ -35,7 +35,6 @@ void turn_toward_sensor_side_left(void) {
 void turn_toward_given_sensor(uint8_t sensor_index) {
     const int16_t sensor_angles[8]={15, 50, 90, 150, -150, -90, -50, -15};
     int16_t number_of_step = (sensor_angles[sensor_index]*9)/2; // integer ratio so no float :) -> experimentally determined
-    motors_init();
     int8_t direction = FORWARD; 
     left_motor_set_pos(0);// reset step counter so we can wait using the while loop
     right_motor_set_pos(0);
@@ -56,7 +55,6 @@ void turn_toward_given_sensor(uint8_t sensor_index) {
 
 void turn_specific_angle(int16_t angle_degrees) {
     int16_t number_of_step = (angle_degrees*9)/2; // integer ratio so no float :) -> experimentally determined
-    motors_init();
     int8_t direction = FORWARD; 
     left_motor_set_pos(0); // reset step counter so we can wait using the while loop
     right_motor_set_pos(0);
@@ -66,10 +64,10 @@ void turn_specific_angle(int16_t angle_degrees) {
     left_motor_set_speed(direction*(TURNINGPACE));
     right_motor_set_speed(direction*(-TURNINGPACE));
     while(left_motor_get_pos() != number_of_step){
-        chThdSleepMilliseconds(1);// dont block other threads while turning
+        chThdSleepMilliseconds(2);// dont block other threads while turning
     }
     while(right_motor_get_pos() != -number_of_step){
-        chThdSleepMilliseconds(1);// dont block other threads while turning
+        chThdSleepMilliseconds(2);// dont block other threads while turning
     }
     left_motor_set_speed(0);
     right_motor_set_speed(0); 
@@ -77,7 +75,6 @@ void turn_specific_angle(int16_t angle_degrees) {
 
 
 void go_forward(int16_t vitesse) {
-    motors_init();
 
     left_motor_set_pos(0); // reset the step counter
     right_motor_set_pos(0);
